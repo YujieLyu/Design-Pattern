2f163fb40663ae3e3544c3722b640fd7c06485b2
YujieLyu <lyuy4@student.unimelb.edu.au>
2020-01-14 19:49:46 +1100

    HackerRank Challenge

---
+++ src/CheckPrime/Prime.java
@@ -0,0 +1,39 @@
+package CheckPrime;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Prime {
+    public void checkPrime(Integer ... a){
+
+        ArrayList<Integer> primes=new ArrayList<>();
+        for(int e:a){
+            if (isPrime(e)){
+                primes.add(e);
+            }
+        }
+        int len=primes.size();
+        if (len==0){
+            System.out.println(" ");
+        }
+        for (int p=0;p<len;p++){
+            if (primes.get(p)!=0){
+                System.out.print(primes.get(p)+" ");
+                if (p==len-1){
+                    System.out.print("\n");
+                }
+            }
+
+        }
+    }
+
+    public boolean isPrime(int num){
+        int count=0;
+        for(int i=1;i<=num;i++){
+            if (num%i==0){
+                count+=1;
+            }
+        }
+        return count==2;
+    }
+}
