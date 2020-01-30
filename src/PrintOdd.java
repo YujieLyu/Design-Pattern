78cdfab458355efcef9331fb0f704fba68519a6d
YujieLyu <lyuy4@student.unimelb.edu.au>
2020-01-30 13:24:06 +1100

    Day6

---
+++ src/PrintOdd.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class PrintOdd {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String s;
+
+        while (scan.hasNextLine()) {
+            s = scan.nextLine();
+
+            if (!isNum(s)) {
+                StringBuilder odd = new StringBuilder();
+                StringBuilder even = new StringBuilder();
+                for (int i = 0; i < s.length(); i++) {
+                    String add=String.valueOf(s.charAt(i));
+                    if (i % 2 == 0) {
+                        even.append(add);
+                    } else {
+                        odd.append(add);
+                    }
+                }
+                System.out.println(even + " " + odd);
+
+            }
+        }
+
+        scan.close();
+    }
+
+
+    private static boolean isNum(String s) {
+        String pattern = "\\d+";
+        return Pattern.matches(pattern, s);
+    }
+}
+
